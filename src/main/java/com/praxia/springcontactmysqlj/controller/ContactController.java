fb847bb5f7788601664665672f2024240ec3e1b7
Eurico Neto <euriconascimentofn@gmail.com>
2021-08-16 17:53:22 -0300

    Criado o projeto

--- src/main/java/com/praxia/springcontactmysqlj/controller/ContactController.java
+++ src/main/java/com/praxia/springcontactmysqlj/controller/ContactController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping({"/spring-contact-mysql-j"})
+@RequestMapping({"/contact"})
 public class ContactController {
     private IContactRepository repository;
 
